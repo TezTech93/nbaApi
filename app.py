641214aab98b4043bc1bfe0daab95bc0be236438
TezTech93 <Shawnteztech93@gmail.com>
2025-02-02 14:20:30 -0600

    nba gameline edit

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import sys, os
+sys.path.append(os.path.dirname((__file__)) + "/nflFiles/")
+from nbaGameLines import *
+from nbaGetData import *
+from nbaStats import *
+
+
+app = FastAPI()
+
+@app.get("/nba/gamelines")
+def get_lines():
+    return {"Data":nba_game_lines}
+
+@app.get("/nba/{team}/{year}")
+def get_team_stats(team,year):
+    results = []
+    #return a list of game data for specified year
+
+@app.get("/nba/{player}/")
+def get_player_stats(range):
+    return 1
+
+@app.get("/nba/{coach}/")
+def get_coach_stats(range):
+    return 1
